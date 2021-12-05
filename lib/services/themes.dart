814f5a0a50655a9aabad69a4aa05dce5d531be71
Stephanie Scullion <stephanielarascullion@yahoo.com>
2021-12-05 21:05:13 +0000

    Add remaning colours to theme service

--- lib/services/themes.dart
+++ lib/services/themes.dart
@@ -9,10 +9,22 @@ class ThemeService {
 
   ThemeService._internal();
 
-  static const primary = Color(0xff304ffe);
-  static const primaryLight = Color(0xff7a7cff);
-  static const dark = Color(0xff000000);
   static const light = Color(0xffffffff);
+  static const dark = Color(0xff000000);
+
+  static const primaryLight = Color(0xff7a7cff);
+  static const primary = Color(0xff304ffe);
+  static const primaryDark = Color(0xff0026ca);
+
+  static const secondaryLight = Color(0xffff795d);
+  static const secondary = Color(0xfff44332);
+  static const secondaryDark = Color(0xffb90007);
+
+  static const primaryAccentLight = Color(0xff80e27e);
+  static const primaryAccent = Color(0xff4caf50);
+  static const primaryAccentDark = Color(0xff087f23);
+
+  static const secondaryAccent = Color(0xffffff00);
 
   static const mediumText = TextStyle(
     fontFamily: 'ArimaMadurai',
