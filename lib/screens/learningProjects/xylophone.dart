d1c429f0e0ca8755c1c0c4a1cee35eefcc4de1bf
Stephanie Scullion <stephanielarascullion@yahoo.com>
2021-12-07 19:37:26 +0000

    Add xylophone music files and implement keys in UI

--- lib/screens/learningProjects/xylophone.dart
+++ lib/screens/learningProjects/xylophone.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Xylophone extends StatelessWidget {
   const Xylophone({Key? key}) : super(key: key);
@@ -7,13 +8,44 @@ class Xylophone extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[800],
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {},
-              child: const Text('Press'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              XylophoneNoteButton(Colors.red, 1),
+              XylophoneNoteButton(Colors.orange, 2),
+              XylophoneNoteButton(Colors.yellow, 3),
+              XylophoneNoteButton(Colors.green, 4),
+              XylophoneNoteButton(Colors.blue, 5),
+              XylophoneNoteButton(Colors.indigo, 6),
+              XylophoneNoteButton(Colors.purple, 7),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class XylophoneNoteButton extends StatelessWidget {
+  const XylophoneNoteButton(this.color, this.note, {Key? key})
+      : super(key: key);
+
+  final Color color;
+  final int note;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$note.wav');
+        },
+        child: const Text(''),
+        style: TextButton.styleFrom(
+          backgroundColor: color,
         ),
       ),
     );
